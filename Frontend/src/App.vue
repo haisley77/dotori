97c10f7139d5edfcbb95e58ea6a147283c307788
jinooyang <jinooyang@gmail.com>
2024-01-16 12:37:05 +0900

    fix routes

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -3,9 +3,5 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'App'
-})
 </script>
---
+++ Frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>hello 김재찬 바보</h1>
+</template>
+
+<style scoped>
+
+</style>
