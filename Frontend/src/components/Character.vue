87e192e624029ee716f67737f5da2a9a1d464b62
이하은 <hahi1108@gmail.com>
2024-01-16 17:55:56 +0900

    feat : 방 생성 모달 책 및 역할 정보 조회 기능

---
+++ Frontend/src/components/Character.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+      <div class="text-h6">거북이</div>
+    </q-card>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .my-card{
+    width: 100%;
+    max-width: 100px;
+    color: black;
+  }
+</style>
