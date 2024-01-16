8a9f10169e1ab7a01e3cf09fb2f17f1a9f0afbaa
문준형 <moonabcd@naver.com>
2024-01-16 14:56:05 +0900

    design : 로그인 UI 디자인추가, 홈페이지 시작버튼추가

---
+++ Frontend/src/pages/LoginPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card bg-white text-black">
+            <q-card-section>
+                <div class="text-h6">지금 도토리를 시작해 보세요</div>
+                <div class="text-h4">간편하게 SNS ID로 시작하기</div>
+            </q-card-section>
+
+            <q-card-actions>
+                <div class="select_btn-group">
+                    <q-btn flat>
+                        <img src="../assets/login/login_btn_kakao.svg" />
+                    </q-btn>
+                    <q-btn flat>
+                        <img src="../assets/login/login_btn_naver.svg" />
+                    </q-btn>
+                </div>
+            </q-card-actions>
+        </q-card>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,8 +1,22 @@
 const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-  }
-]
+    {
+        path: '/',
+        component: () => import('layouts/MainLayout.vue'),
+        // children: [
+        //     {
+        //         path: '',
+        //         component: () => import('pages/HomePage.vue'),
+        //     },
+        //     {
+        //         path: 'login',
+        //         component: () => import('pages/LoginPage.vue'),
+        //     },
+        // ],
+    },
+    {
+        path: '/login',
+        component: () => import('pages/LoginPage.vue'),
+    },
+];
 
-export default routes
+export default routes;
