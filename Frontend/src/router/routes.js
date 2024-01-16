30948182e7b41b0ac8a9bf8524307170531efc7d
SeokHyun Cho <doongdang@naver.com>
2024-01-16 17:23:20 +0900

    feat : 방 녹화 페이지 router

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -2,6 +2,10 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
+  },
+  {
+    path: '/room-recording',
+    component: () => import('pages/RoomRecording.vue'),
   }
 ]
 
