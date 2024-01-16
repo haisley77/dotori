10832bebbb12067a0b4535dd44c93ab8e411193d
문준형 <moonabcd@naver.com>
2024-01-16 14:56:05 +0900

    design : 로그인 UI 디자인추가, 홈페이지 시작버튼추가

--- Frontend/src/layouts/MainLayout.vue
+++ Frontend/src/layouts/MainLayout.vue
@@ -1,10 +1,9 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
+    <div>
+        <q-btn color="primary" label="시작하기" router-link to="/login" />
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ Frontend/src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <q-btn color="primary" label="시작하기" />
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
