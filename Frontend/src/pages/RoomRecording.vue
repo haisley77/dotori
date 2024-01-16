b0609471153310fd35d3def0c96dbe700b392d47
SeokHyun Cho <doongdang@naver.com>
2024-01-16 17:24:38 +0900

    feat : 방 녹화 페이지 Layout 씬 리스트, 씬+대사(scene-info), 제어 버튼(controller) 레이아웃 구성

---
+++ Frontend/src/pages/RoomRecording.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="entire-container row">
+    <div class="left-container col-2">
+      <div class="scene-list-container">
+
+      </div>
+    </div>
+    <div class="right-container q-pa-sm col-10">
+      <div class="scene-info-and-controller-container">
+        <div class="scene-info-container">
+
+        </div>
+        <div class="controller-container">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.entire-container{
+  height: 100vh;
+  border: green solid 1px;
+}
+
+.left-container{
+  height: 100%;
+  border: red solid 1px;
+}
+
+.right-container{
+  height: 100%;
+  border: blue solid 1px;
+}
+
+.scene-info-and-controller-container{
+  height: 100%;
+  border: aqua solid 1px;
+}
+
+.scene-info-container{
+  height: 90%;
+  border: black solid 1px;
+}
+
+
+.controller-container{
+  height: 10%;
+  border: black solid 1px;
+}
+
+</style>
