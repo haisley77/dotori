e24e1d1a34964091750e069a4e4e5023f5a3ba11
yeonmi <ar77gt@daum.net>
2024-01-17 11:37:40 +0900

    add : breakpoints

--- Frontend/src/pages/Breakpoints.vue
+++ Frontend/src/pages/Breakpoints.vue
@@ -3,6 +3,7 @@
     <section class="q-mb-xl">
       <div class="text-h4">Breakpoints</div>
       <q-separator class="q-my-md"/>
+      <div class="target"></div>
     </section>
   </q-page>
 </template>
@@ -12,5 +13,16 @@
 </script>
 
 <style lang="scss" scoped>
+.target{
+  height:200px;
+  background-color: darkcyan;
+}
 
+// 일관성을 맞추기 위해 sass 변수 사용
+// maxwidth가 500px일때 배경색이 빨강이 되도록.
+@media(max-width:500px){
+  .target{
+    background-color: red;
+  }
+}
 </style>
