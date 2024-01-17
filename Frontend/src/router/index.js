acc2533784319c05176174d472c7282a77255895
정연미 <ar77gt@daum.net>
2024-01-17 09:12:28 +0900

    style : add files

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,24 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Roomlist from '../pages/Roomlist.vue';
-import Booklist from '../pages/Booklist.vue';
-
-const routes = [
-  {
-    path: '/',
-    name: 'Roomlist',
-    component: Roomlist
-  },
-
-  {
-    path: '/Booklist',
-    name: 'Booklist',
-    component: Booklist,
-  }
-];
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-});
-
-export default router;
+962 B
+import { route } from 'quasar/wrappers'
+import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import routes from './routes'
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Router instance.
+ */
+export default route(function (/* { store, ssrContext } */) {
+  const createHistory = process.env.SERVER
+    ? createMemoryHistory
+    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+  const Router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes,
+    // Leave this as is and make changes in quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    history: createHistory(process.env.VUE_ROUTER_BASE)
+  })
+  return Router
+})
--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,7 +1,7 @@
 const routes = [
   {
-    // path: '/',
-    // component: () => import('layouts/MainLayout.vue'),
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
   }
 ]
 
