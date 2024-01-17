1b2dbe85eabf2a68c109e407a9195f9f0475dda0
yeonmi <ar77gt@daum.net>
2024-01-17 16:13:47 +0900

    feat : q-list 추가

--- Frontend/src/layouts/List/ListDrawer.vue
+++ Frontend/src/layouts/List/ListDrawer.vue
@@ -1,11 +1,66 @@
 <template>
-  <div>
-    ListDrawer
-  </div>
+  <q-drawer v-model="drawer" :width="240">
+    <q-scroll-area class="fit">
+          <q-list>
+
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
+                </q-item-section>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+            </template>
+
+          </q-list>
+        </q-scroll-area>
+  </q-drawer>
 </template>
 
 <script setup>
-
+import {ref} from 'vue';
+const drawer = ref(true);
+const menuList = [
+  {
+    icon: 'inbox',
+    label: 'Inbox',
+    separator: true
+  },
+  {
+    icon: 'send',
+    label: 'Outbox',
+    separator: false
+  },
+  {
+    icon: 'delete',
+    label: 'Trash',
+    separator: false
+  },
+  {
+    icon: 'error',
+    label: 'Spam',
+    separator: true
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+    separator: false
+  },
+  {
+    icon: 'feedback',
+    label: 'Send Feedback',
+    separator: false
+  },
+  {
+    icon: 'help',
+    iconColor: 'primary',
+    label: 'Help',
+    separator: false
+  }
+]
 </script>
 
 <style lang="scss" scoped>
