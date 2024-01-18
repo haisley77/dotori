f0fb2f2f51417df0ae0687ab6de8cc5690133c14
jh <bndgc@naver.com>
2024-01-18 17:28:21 +0900

    Add: routes 추가

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -2,7 +2,25 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-  }
-]
+  },
+  {
+    path: '/my-page',
+    component: () => import('layouts/MyPageLayout.vue'),
+    children: [
+      {
+        path: 'info',
+        component: () => import('pages/MyInfoPage.vue'),
+      },
+      {
+        path: 'collection',
+        component: () => import('pages/MyVideoCollectionPage.vue'),
+      },
+      {
+        path: 'avatar',
+        component: () => import('pages/MyAvatarPage.vue'),
+      }
+    ],
+  },
+];
 
-export default routes
+export default routes;
