f7e6ff5ab93685d913cb4dfba7ea2879e78d1dfb
yeonmi <ar77gt@daum.net>
2024-01-18 13:36:57 +0900

    feat : add dialog

--- Frontend/src/pages/List/BookIndexPage.vue
+++ Frontend/src/pages/List/BookIndexPage.vue
@@ -51,7 +51,43 @@
                 저자 : 김흥돌
               </div>
 
-              <q-btn push color="primary" size="" label="상세보기"/>
+
+              <!-- Dialog -->
+              <div class="q-pa-md q-gutter-sm">
+                <q-btn label="Maximized" color="primary" @click="dialog = true" />
+
+                <q-dialog
+                  v-model="dialog"
+                  persistent
+                  :maximized="maximizedToggle"
+                  transition-show="slide-up"
+                  transition-hide="slide-down"
+                >
+                  <q-card class="bg-primary text-white">
+                    <q-bar>
+                      <q-space />
+
+                      <q-btn dense flat icon="minimize" @click="maximizedToggle = false" :disable="!maximizedToggle">
+                        <q-tooltip v-if="maximizedToggle" class="bg-white text-primary">Minimize</q-tooltip>
+                      </q-btn>
+                      <q-btn dense flat icon="crop_square" @click="maximizedToggle = true" :disable="maximizedToggle">
+                        <q-tooltip v-if="!maximizedToggle" class="bg-white text-primary">Maximize</q-tooltip>
+                      </q-btn>
+                      <q-btn dense flat icon="close" v-close-popup>
+                        <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+                      </q-btn>
+                    </q-bar>
+
+                    <q-card-section>
+                      <div class="text-h6">Alert</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+                    </q-card-section>
+                  </q-card>
+                </q-dialog>
+              </div>
 
 
 
@@ -63,8 +99,17 @@
   </q-page>
 </template>
 
-<script setup>
+<script>
+import { ref } from 'vue'
 
+export default {
+  setup () {
+    return {
+      dialog: ref(false),
+      maximizedToggle: ref(true)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
