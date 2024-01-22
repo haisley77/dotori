0aed2dec940d8b575dcaa9251e2b563e5e76fe0a
SeokHyun Cho <doongdang@naver.com>
2024-01-22 15:25:17 +0900

    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 ################ # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    #   [ 커밋 타입 목록 ]
    #   feat : 기능 (새로운 기능)
    #   fix : 버그 (버그 수정)
    #   design : CSS 등 사용자 UI 디자인 변경
    #   refactor : 리팩토링
    #   style : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs : 문서 (문서 추가, 수정, 삭제)
    #   add : 코드나 테스트, 예제, 문서등의 추가 생성이 있는 경우
    #   test : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore : 기타 변경사항 (빌드 스크립트 수정ea 등)
    #   post : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    #   rename : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #   remove : 파일을 삭제하는 작업만 수행한 경우
    #   move : 코드의 이동이 있는 경우
    #   comment : 필요한 주석 추가 및 변경
    #   update : 계정이나 버전 업데이트가 있는 경우
    ################

---
+++ Frontend/src/pages/RoomMakingModal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="book">
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card">
+        <img class="book-image" src="https://cdn.quasar.dev/img/mountains.jpg">
+        <div class="text-h6">{{ name }}</div>
+      </q-card>
+    </div>
+    <div>
+      <q-card dark bordered class="bg-yellow-7 book-info book-story">
+        <q-card-section>
+          <div class="text-h6">토끼와 거북이</div>
+          <div class="text-subtitle2">도토리 오리지널</div>
+        </q-card-section>
+
+        <q-separator dark inset />
+
+        <q-card-section>
+          옛날 옛적에, 토끼와 거북이가 살고 있었다.
+          토끼는 매우 빨랐고, 거북이는 매우 느렸다.
+          어느날 토끼가 거북이를 느림보라고 놀려대자, 거북이는 자극을 받고 토끼에게 달리기 경주를 제안하였다.
+          경주를 시작한 토끼는 거북이가 한참 뒤쳐진 것을 보고 안심을 하고 중간에 낮잠을 잔다.
+          그런데 토끼가 잠을 길게 자자 거북이는 토끼를 지나친다.
+          잠에서 문득 깬 토끼는 거북이가 자신을 추월했다는 사실을 깨닫게 되고 빨리 뛰어가보지만 결과는 거북이의 승리였다.
+        </q-card-section>
+      </q-card>
+      <q-card dark bordered class="bg-yellow-5 book-info book-roles">
+        <q-card-section>
+          <div class="text-h6">역할 소개</div>
+        </q-card-section>
+
+        <q-separator dark inset />
+
+        <q-card-section>
+          <div class="roles">
+            <Character v-for="index in 5" :key="index" />
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Character from 'components/Character.vue';
+  import {defineComponent} from 'vue';
+
+  export default defineComponent({
+    components: {Character},
+  });
+
+</script>
+
+<style scoped>
+.book-info{
+  width: 100%;
+  max-width:600px;
+  color: black;
+}
+.roles{
+  display:flex;
+}
+.book{
+  display:flex;
+}
+.book-image{
+  width: 100%;
+  max-width:200px;
+  height:450px;
+}
+</style>
