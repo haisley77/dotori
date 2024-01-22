b71739532d731b465e7f70bf904223fd77e371c5
SeokHyun Cho <doongdang@naver.com>
2024-01-22 15:34:09 +0900

    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 ################ # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    #   [ 커밋 타입 목록 ]
    #   feat : 기능 (새로운 기능)
    #   fix : 버그 (버그 수정)
    #   design : CSS 등 사용자 UI 디자인 변경
    #   refactor : 리팩토링
    #   style : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs : 문서 (문서 추가, 수정, 삭제)
    #   add : 코드나 테스트, 예제, 문서등의 추가 생성이 있는 경우
    #   test : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore : 기타 변경사항 (빌드 스크립트 수정ea 등)
    #   post : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    #   rename : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #   remove : 파일을 삭제하는 작업만 수행한 경우
    #   move : 코드의 이동이 있는 경우
    #   comment : 필요한 주석 추가 및 변경
    #   update : 계정이나 버전 업데이트가 있는 경우
    ################

--- Frontend/src/layouts/MainLayout.vue
+++ Frontend/src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
     <Header />
-    <div class="q-pa-md">
+    <div class="q-md">
         <q-carousel
             arrows
             infinite
@@ -8,7 +8,7 @@
             navigation
             animated
             v-model="slide"
-            height="570px"
+            height="600px"
         >
             <q-carousel-slide
                 name="first"
@@ -63,18 +63,28 @@
             </q-carousel-slide>
         </q-carousel>
     </div>
-    <div class="q-pa-md">
-        <q-parallax :height="700" :speed="1.2">
+    <div class="q-md">
+        <q-parallax :height="900" :speed="0.3">
             <template v-slot:media>
-                <img src="https://cdn.quasar.dev/img/parallax1.jpg" />
+                <img src="../assets/login/loginbackground.png" />
             </template>
 
-            <h1 class="text-white">Docks</h1>
+            <div
+                ref="animatedText"
+                class="absolute-top text-left text-black text-h3 text2"
+            >
+                왜 도토리에요?
+            </div>
         </q-parallax>
     </div>
     <div class="col-6">
         <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-            <div class="absolute-top text-subtitle1 text-center">Caption</div>
+            <div
+                ref="animatedText2"
+                class="absolute-top text-h3 text-left text3"
+            >
+                다양한 탈
+            </div>
         </q-img>
     </div>
     <div>
@@ -84,49 +94,88 @@
 
 <script setup>
     import Header from 'layouts/Header.vue';
-    import {ref} from 'vue';
+    import {ref, onMounted, watchEffect} from 'vue';
 
     const slide = ref('first');
+    //메인페이지 캐러셀 자동반복
     const autoplay = ref(true);
+    //화면안에 요소가 들어오면 아래에서 올라오기
+    const animatedTextRef = ref(null);
+    const animatedTextRef2 = ref(null);
 
-    // const bounceImage = el => {
-    //     // in this example, when the `<div>` comes into view,
-    //     // we bounce it for 2 seconds
+    onMounted(() => {
+        // animatedTextRef.value에 직접 DOM 요소를 할당
+        animatedTextRef.value = document.querySelector('.text2');
+        animatedTextRef2.value = document.querySelector('.text3');
+    });
 
-    //     el.classList.add('animate-bounce');
+    const observeElement = element => {
+        if (element instanceof Element) {
+            const observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        element.classList.add('entered');
+                    } else {
+                        element.classList.remove('entered');
+                    }
+                });
+            });
 
-    //     setTimeout(() => {
-    //         // we make sure the node is still in DOM
-    //         // (user hasn't navigated away from the Vue component
-    //         // rendering our `<div>`)
-    //         // so we don't generate an error
-    //         if (document.body.contains(el)) {
-    //             // then remove the helper class to
-    //             // stop bouncing
-    //             el.classList.remove('animate-bounce');
-    //         }
-    //     }, 2000);
-    // };
+            observer.observe(element);
+        }
+    };
+    watchEffect(() => {
+        observeElement(animatedTextRef.value);
+        observeElement(animatedTextRef2.value);
+    });
 </script>
 
-<!--<style lang="sass" scoped>-->
-<!--.custom-caption-->
-<!--  text-align: center-->
-<!--  padding: 400px-->
-<!--  color: text-black-->
+<style lang="scss" scoped>
+    .custom-caption {
+        text-align: center;
+        padding: 12px;
+        color: white;
+    }
 
-<!--.custom-button-->
-<!--      text-align: center-->
-<!--      padding: 70px-->
+    .custom-button {
+        text-align: center;
+        padding: 70px;
+    }
 
-<!--  .animate-bounce-->
-<!--      animation: q-bounce 2s infinite-->
+    .custom-caption {
+        text-align: center;
+        padding: 400px;
+        color: black;
+    }
 
-<!--    @keyframes q-bounce-->
-<!--      0%, 20%, 50%, 80%, 100%-->
-<!--        transform: translateY(0)-->
-<!--      40%-->
-<!--        transform: translateY(-30px)-->
-<!--      60%-->
-<!--        transform: translateY(-15px)-->
-<!--</style>-->
+    .custom-button {
+        text-align: center;
+        padding: 70px;
+    }
+    // 글씨 나타나기
+    .text2 {
+        padding: 200px;
+        opacity: 0;
+        transform: translateY(20px);
+        transition: opacity 1s ease-in-out, transform 1s ease-in-out;
+    }
+
+    .text2.entered {
+        opacity: 1;
+        transform: translateY(0);
+    }
+    // .text3 {
+    // padding: 20px;
+    // opacity: 0;
+    // transform: translateY(20px);
+    // transition: opacity 1s ease-in-out, transform 1s ease-in-out;
+    // }
+
+    // .text3.entered {
+    // opacity: 1;
+    // transform: translateY(0);
+    // }
+    .q-img__content > div {
+        background: none !important; /* 배경을 없애는 스타일 */
+    }
+</style>
