c4de13546c863ce37ff1f1d87412c246b3750cfa
yeonmi <ar77gt@daum.net>
2024-01-22 15:28:55 +0900

    feat: settings

--- backend/src/main/java/jpa/Member.java
+++ backend/src/main/java/jpa/Member.java
@@ -1,38 +1,22 @@
 package jpa;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.*;
 
-//jpa 가 관리하는 객체 , db 테이블과 매핑해서 사용한다.
+import javax.persistence.*;
+
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_memo")
 @Entity
-//@Table(name="USER") // 테이블을 다른 이름으로 매핑하고 싶을때. db의 user 테이블과 매핑한다.
 public class Member {
-    @Id // PK 를 알려준다.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL의 AUTO_INCREMENT를 사용
     private Long id;
-    private String name;
-    private int age;
 
-    public Member(){
-    }
-
-    public Member(Long id, String name){
-        this.id = id;
-        this.name = name;
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Column(length = 200, nullable = false)
+    private String memoText;
 }
