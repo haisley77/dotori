156b08c2ddc78c69472bb254087f430c2e5d86bc
jh <bndgc@naver.com>
2024-01-24 13:51:11 +0900

    move: domain 생성 및 기존 package 이동

---
+++ backend/src/main/java/com/dotori/backend/domain/video/model/entity/SceneVideo.java
@@ -0,0 +1,39 @@
+package com.dotori.backend.domain.video.model.entity;
+
+import static javax.persistence.FetchType.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.dotori.backend.common.entity.BaseTimeEntity;
+import com.dotori.backend.domain.room.model.entity.Room;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "scene_video")
+public class SceneVideo extends BaseTimeEntity {
+	@Id
+	@Column(name = "scene_video_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long sceneVideoId;
+
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "room_id", nullable = false)
+	private Room room;
+
+	@Column(name = "scene_order")
+	private int sceneOrder;
+
+	@Column(name = "path")
+	private String path;
+}
