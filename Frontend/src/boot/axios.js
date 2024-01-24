32315db427f782db54ff1e7875dae6beaf910393
문준형 <moonabcd@naver.com>
2024-01-24 16:31:23 +0900

    add: !잘못된! 프론트 코드 수정2

---
+++ Frontend/src/boot/axios.js
@@ -0,0 +1,12 @@
+import {boot} from 'quasar/wrappers';
+import axios from 'axios';
+
+const api = axios.create({baseURL: 'http://localhost:8080'});
+
+export default boot(({app}) => {
+    app.config.globalProperties.$axios = axios;
+
+    app.config.globalProperties.$api = api;
+});
+
+export {api};
