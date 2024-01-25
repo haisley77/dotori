f536ca2db3af09611db1d83796ac6b0c3b24b5ad
이하은 <hahi1108@gmail.com>
2024-01-25 14:26:13 +0900

    feat : 방ãëëë° 방(session) 생성

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,15 +1,45 @@
 package com.dotori.backend.domain.room.service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import com.dotori.backend.domain.room.repository.RoomRepository;
 
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Service
+@NoArgsConstructor
 @AllArgsConstructor
-public class RoomServiceImpl {
+public class RoomServiceImpl implements RoomService {
 
 	private RoomRepository roomRepository;
 
+	@Override
+	public Session createSession(OpenVidu openvidu, Map<String, Object> sessionProperties) throws
+		Exception {
+		SessionProperties properties = SessionProperties.fromJson(sessionProperties).build();
+		// System.out.println(properties.toString());
+		return openvidu.createSession(properties);
+	}
+
+	@Override
+	public String saveRoomInfo(Map<String, Object> roomInfo, String sessionId) {
+		return null;
+	}
+
+	@Override
+	public Session findBySessionId(String sessionId) {
+		return null;
+	}
+
+	@Override
+	public Connection createConnection(Map<String, Object> connectionProperties) {
+		return null;
+	}
 }
