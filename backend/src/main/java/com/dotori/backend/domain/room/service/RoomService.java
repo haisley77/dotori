df7d066784472fb5ac5e77c90a71880f13658a4a
이하은 <hahi1108@gmail.com>
2024-01-25 14:23:46 +0900

    feat : 방 ㅓ서비스 메소드 정의

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomService.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomService.java
@@ -1,5 +1,19 @@
 package com.dotori.backend.domain.room.service;
 
+import java.util.Map;
+
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.Session;
+
 public interface RoomService {
+
+	Session createSession(OpenVidu openvidu, Map<String, Object> sessionProperties) throws Exception;
+
+	String saveRoomInfo(Map<String, Object> roomInfo, String sessionId) throws Exception;
+
+	Session findBySessionId(String sessionId) throws Exception;
+
+	Connection createConnection(Map<String, Object> connectionProperties) throws Exception;
 
 }
