6ccff8cdd9eac6cbf00f154fc93673907c6cbd45
이하은 <hahi1108@gmail.com>
2024-01-25 22:13:23 +0900

    comment : 방 생ì세션 및 커넥션 생성(방장) service 함계층 메서드 주석 추가

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -17,23 +17,34 @@ import io.openvidu.java.client.OpenVidu;
 import io.openvidu.java.client.Session;
 import io.openvidu.java.client.SessionProperties;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 @Service
-@NoArgsConstructor
 @AllArgsConstructor
 public class RoomServiceImpl implements RoomService {
 
-	private RoomRepository roomRepository;
+	private final RoomRepository roomRepository;
 
+	/**
+	 * 세션을 생성하는 메서드
+	 * @param openvidu openvidu 객체
+	 * @param sessionProperties session 설정 정보를 담은 객체
+	 * @return session 객체
+	 * @throws Exception session 생성 시 발생하는 예외 객체
+	 */
 	@Override
-	public Session createSession(OpenVidu openvidu, Map<String, Object> sessionProperties) throws
-		Exception {
+	public Session createSession(OpenVidu openvidu,
+		Map<String, Object> sessionProperties) throws Exception {
 		SessionProperties properties = SessionProperties.fromJson(sessionProperties).build();
 		// System.out.println(properties.toString());
 		return openvidu.createSession(properties);
 	}
 
+	/**
+	 * 방 정보와 세션 아이디를 저장하는 메서드
+	 * @param roomInfo 방 정보
+	 * @param sessionId 세션 아이디
+	 * @return DB에 저장한 방 pk
+	 */
 	@Override
 	public Long saveRoomInfo(Map<String, Object> roomInfo, String sessionId) {
 		List<RoomMember> roomMembers = new ArrayList<>();
@@ -41,14 +52,14 @@ public class RoomServiceImpl implements RoomService {
 		// 책 정보 더미데이터
 		Book book = Book.builder()
 			.title("토끼와 거북이")
-			.bookImg("토끼와거북이주소")
+			.bookImg("토끼와 거북이 책 이미지 주소")
 			.author("이하은")
 			.build();
 
 		// room 엔티티를 생성합니다.
 		Room room = Room.builder()
 			.book(book)
-			.hostId((Long)roomInfo.get("hostId"))
+			.hostId(1L)
 			.roomMembers(roomMembers)
 			.title("토끼와 거북이 같이 연극해요!")
 			.password("1234")
@@ -56,20 +67,33 @@ public class RoomServiceImpl implements RoomService {
 			.sessionId(sessionId)
 			.build();
 
-		// DB에 저장하러 갑니다.
+		// DB에 저장하고 방 pk를 반환합니다.
 		return roomRepository.saveRoomInfo(room);
 	}
 
+	/**
+	 * 방 pk로 해당하는 세션을 찾는 메서드
+	 * @param openvidu openvidu 객체
+	 * @param roomId 방 pk
+	 * @return session 객체
+	 */
 	@Override
 	public Session findSessionByRoomId(OpenVidu openvidu, Long roomId) {
 		String sessionId = roomRepository.findSessionByRoomId(roomId);
 		return openvidu.getActiveSession(sessionId);
 	}
 
+	/**
+	 * 방 pk에 해당하는 세션과 커넥션을 생성하는 메서드(방장)
+	 * @param openvidu openvidu 객체
+	 * @param roomId 방 pk
+	 * @param connectionProperties connection 설정 정보를 담은 객체
+	 * @return connection 객체
+	 * @throws Exception connection 생성 시 발생하는 예외 객체
+	 */
 	@Override
 	public Connection createConnectionByHost(OpenVidu openvidu, Long roomId,
-		Map<String, Object> connectionProperties) throws
-		Exception {
+		Map<String, Object> connectionProperties) throws Exception {
 		Session session = findSessionByRoomId(openvidu, roomId);
 		if (session == null) {
 			//return new ResponseEntity<>(HttpStatus.NOT_FOUND);
