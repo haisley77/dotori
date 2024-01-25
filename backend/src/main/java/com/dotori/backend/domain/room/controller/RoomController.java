3ca3baca17b368618278f582c481b64dc36683be
이하은 <hahi1108@gmail.com>
2024-01-25 09:25:27 +0900

    feat : ëopenvidu 활용 방 활용한 ë°session 생성, connection 생성 RoomController

---
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -0,0 +1,77 @@
+package com.dotori.backend.domain.room.controller;
+
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.ConnectionProperties;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
+
+@CrossOrigin(origins = "*")
+@RestController("/")
+@PropertySource("classpath:application-openvidu.yml")
+@ConfigurationProperties(prefix = "openvidu")
+public class RoomController {
+
+	@Value("${url}")
+	private String OPENVIDU_URL;
+
+	@Value("${secret}")
+	private String OPENVIDU_SECRET;
+
+	private OpenVidu openvidu;
+
+	@PostConstruct
+	public void init() {
+		this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+	}
+
+	/**
+	 * @param params The Session properties
+	 * @return The Session ID
+	 */
+	@PostMapping("/api/sessions")
+	public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+		throws OpenViduJavaClientException, OpenViduHttpException {
+		SessionProperties properties = SessionProperties.fromJson(params).build();
+		System.out.println(properties.toString());
+		Session session = openvidu.createSession(properties);
+		return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+	}
+
+	/**
+	 * @param sessionId The Session in which to create the Connection
+	 * @param params    The Connection properties
+	 * @return The Token associated to the Connection
+	 */
+	@PostMapping("/api/sessions/{sessionId}/connections")
+	public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
+		@RequestBody(required = false) Map<String, Object> params)
+		throws OpenViduJavaClientException, OpenViduHttpException {
+		Session session = openvidu.getActiveSession(sessionId);
+		if (session == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+		Connection connection = session.createConnection(properties);
+		System.out.println(connection.getConnectionId());
+		return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+	}
+
+}
