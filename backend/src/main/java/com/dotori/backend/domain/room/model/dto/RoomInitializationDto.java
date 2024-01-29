ddb98347238fd29d923d562616d1ba70984270dd
이하은 <hahi1108@gmail.com>
2024-01-29 23:36:34 +0900

    mod : 방 초기 생성 dto 수정

--- backend/src/main/java/com/dotori/backend/domain/room/model/dto/RoomInitializationDto.java
+++ backend/src/main/java/com/dotori/backend/domain/room/model/dto/RoomInitializationDto.java
@@ -5,14 +5,13 @@ import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoomInitializationDto {
 	private Map<String, Object> sessionProperties;
+	private Map<String, Object> connectionProperties;
 	private Map<String, Object> roomInfo;
 	// private Map<String, Object> connectionProperties;
 }
