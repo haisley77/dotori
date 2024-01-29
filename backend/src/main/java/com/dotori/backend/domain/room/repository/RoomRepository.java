f6729a2d15fddd5d7aef9d0ca14c2007b3643e99
이하은 <hahi1108@gmail.com>
2024-01-30 00:42:19 +0900

    mod : RoomMemberRepsitory 변경

--- backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
+++ backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
@@ -1,15 +1,9 @@
 package com.dotori.backend.domain.room.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.dotori.backend.domain.room.model.entity.Room;
-import com.dotori.backend.domain.room.model.entity.RoomMember;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-	void save(RoomMember roomMember);
 
-	@Query("delete from RoomMember where RoomMember.member.memberId = :memberId")
-	void deleteByRoomMemberId(@Param("memberId") Long memberId);
 }
