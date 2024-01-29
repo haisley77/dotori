3478ca8fce46897fc9f4a512256350c53b68d545
이하은 <hahi1108@gmail.com>
2024-01-29 23:43:58 +0900

    Feat : 방 관련 조회 서비스 메서드 작성

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -113,32 +113,17 @@ public class RoomServiceImpl implements RoomService {
 		return resultData;
 	}
 
-	/**
-	 * 방 pk로 해당하는 세션을 찾는 메서드
-	 * @param openvidu openvidu 객체
-	 * @param roomId 방 pk
-	 * @return session 객체
-	 */
 	@Override
-	public Session findSessionByRoomId(OpenVidu openvidu, Long roomId) throws Exception {
-		String sessionId = roomRepository.findSessionByRoomId(roomId);
-		return openvidu.getActiveSession(sessionId);
+	public Session findSessionByRoomId(OpenVidu openvidu, Long roomId) {
+		Optional<Room> optionalRoom = roomRepository.findById(roomId);
+		if (optionalRoom.isPresent()) {
+			return openvidu.getActiveSession(optionalRoom.get().getSessionId());
+		}
+		throw new RuntimeException("방 조회 불가");
 	}
 
-	/**
-	 * 방 pk에 해당하는 세션과 커넥션을 생성하는 메서드(방장)
-	 * @param openvidu openvidu 객체
-	 * @param roomId 방 pk
-	 * @param connectionProperties connection 설정 정보를 담은 객체
-	 * @return connection 객체
-	 * @throws Exception connection 생성 시 발생하는 예외 객체
-	 */
-	@Override
-	public Connection createConnectionByHost(OpenVidu openvidu, Long roomId,
-		Map<String, Object> connectionProperties) throws Exception {
-		Session session = findSessionByRoomId(openvidu, roomId);
-		ConnectionProperties properties = ConnectionProperties.fromJson(connectionProperties).build();
-		return session.createConnection(properties);
+	public List<Room> getAllRooms() {
+		return roomRepository.findAll();
 	}
 
 	@Override
