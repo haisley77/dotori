6543767c2eb744a8cb86c1b90635188ba6221784
jh <bndgc@naver.com>
2024-01-30 14:48:53 +0900

    feat: get-scene-api   - scene with roles   - scene with scripts

--- backend/src/main/java/com/dotori/backend/domain/book/service/SceneService.java
+++ backend/src/main/java/com/dotori/backend/domain/book/service/SceneService.java
@@ -1,14 +1,19 @@
 package com.dotori.backend.domain.book.service;
 
+import static com.dotori.backend.domain.book.model.dto.BookMapper.*;
 import static java.util.stream.Collectors.*;
 
 import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dotori.backend.domain.book.model.dto.BookMapper;
+import com.dotori.backend.domain.book.model.dto.SceneDetailDto;
 import com.dotori.backend.domain.book.model.dto.SceneDto;
+import com.dotori.backend.domain.book.model.entity.Scene;
 import com.dotori.backend.domain.book.repository.SceneRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -26,5 +31,12 @@ public class SceneService {
 			.stream()
 			.map(BookMapper::toSceneDto)
 			.collect(toList());
+	}
+
+	@Transactional(readOnly = true)
+	public SceneDetailDto getScene(Long sceneId) {
+		Scene scene = sceneRepository.findById(sceneId)
+			.orElseThrow(() -> new EntityNotFoundException("해당하는 scene이 존재하지 않습니다."));
+		return toSceneDetailDto(scene);
 	}
 }
