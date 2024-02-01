4a2c83567e047aa7ebd1e6ace153e32a1b3ea82e
문준형 <moonabcd@naver.com>
2024-02-01 10:15:52 +0900

    rebase: 3

---
+++ backend/src/main/java/com/dotori/backend/domain/member/model/Member.java
@@ -0,0 +1,60 @@
+package com.dotori.backend.domain.member.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.dotori.backend.domain.member.type.Role;
+import com.dotori.backend.domain.member.type.SocialType;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Builder
+@Table(name = "Member")
+@AllArgsConstructor
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	private String email; // 이메일
+	private String nickname; // 닉네임
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Enumerated(EnumType.STRING)
+	private SocialType socialType; // KAKAO, NAVER, GOOGLE
+
+	private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
+
+	private String refreshToken; // 리프레시 토큰
+
+	// 유저 권한 설정 메소드
+	public void authorizeUser() {
+		this.role = Role.USER;
+	}
+
+	//== 유저 필드 업데이트 ==//
+	public void updateNickname(String updateNickname) {
+		this.nickname = updateNickname;
+	}
+
+	public void updateRefreshToken(String updateRefreshToken) {
+		this.refreshToken = updateRefreshToken;
+	}
+}
