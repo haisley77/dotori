17ea5a0b0fda8663549b1200fdda87224c99818c
문준형 <moonabcd@naver.com>
2024-02-01 10:14:21 +0900

    rebase: 2

--- backend/src/main/java/com/dotori/backend/members/model/Member.java
+++ backend/src/main/java/com/dotori/backend/members/model/Member.java
@@ -7,37 +7,54 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-import com.dotori.backend.members.type.MemberType;
+import com.dotori.backend.members.type.Role;
+import com.dotori.backend.members.type.SocialType;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Builder
+@Table(name = "USERS")
 @AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class Member extends BaseEntity {
+public class Member {
 
 	@Id
-	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
 	private Long id;
 
-	@Column(length = 50)
-	private String phoneNum;
+	private String email; // 이메일
+	private String nickname; // 닉네임
 
-	@Column(length = 50)
-	private String memberNickname;
-
-	@Column(length = 50)
-	private String memberEmail;
-
-	@Column(columnDefinition = "ENUM('KAKAO', 'NAVER', 'GOOGLE', 'NORMAL') DEFAULT 'NORMAL'")
 	@Enumerated(EnumType.STRING)
-	private MemberType memberType;
+	private Role role;
 
+	@Enumerated(EnumType.STRING)
+	private SocialType socialType; // KAKAO, NAVER, GOOGLE
+
+	private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
+
+	private String refreshToken; // 리프레시 토큰
+
+	// 유저 권한 설정 메소드
+	public void authorizeUser() {
+		this.role = Role.USER;
+	}
+
+	//== 유저 필드 업데이트 ==//
+	public void updateNickname(String updateNickname) {
+		this.nickname = updateNickname;
+	}
+
+	public void updateRefreshToken(String updateRefreshToken) {
+		this.refreshToken = updateRefreshToken;
+	}
 }
