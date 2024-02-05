283a2e48e54fccd5866bc09645d516c773f4b242
문준형 <moonabcd@naver.com>
2024-02-05 13:02:18 +0900

    chore: merge conflict 해결(testredisconfig 수정)

--- backend/src/test/java/com/dotori/backend/domain/book/TestRedisSessionConfig.java
+++ backend/src/test/java/com/dotori/backend/domain/book/TestRedisSessionConfig.java
@@ -1,5 +1,6 @@
 package com.dotori.backend.domain.book;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,20 +8,15 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 
 @TestConfiguration
 public class TestRedisSessionConfig {
+	@Value("${spring.redis.host}")
+	private String REDIS_HOST;
+
+	@Value("${spring.redis.port}")
+	private String REDIS_PORT;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory();
+		return new LettuceConnectionFactory(REDIS_HOST, Integer.parseInt(REDIS_PORT));
 	}
-	// @Bean
-	// public SessionRepositoryFilter<MapSession> sessionRepositoryFilter(
-	//     SessionRepository<MapSession> sessionRepository) {
-	//     SessionRepositoryFilter<MapSession> sessionRepositoryFilter = new SessionRepositoryFilter<>(sessionRepository);
-	//     return sessionRepositoryFilter;
-	// }
 
-	// @Bean
-	// public SessionRepository<MapSession> sessionRepository() {
-	//     MapSessionRepository sessionRepository = new MapSessionRepository(new ConcurrentHashMap<>());
-	//     return sessionRepository;
-	// }
 }
