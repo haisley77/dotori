bd7f7d165681f35d309c82783f94d317e12ea31d
jinooyang <jinooyang@gmail.com>
2024-02-05 19:31:18 +0900

    design : mainpage

--- Frontend/src/components/MainPageComponents/ElementOne.vue
+++ Frontend/src/components/MainPageComponents/ElementOne.vue
@@ -1,32 +1,32 @@
 <template>
-  <div>
-    <p v-for="(item, index) in items" :key="index">
-      {{ item.name }} - {{ item.value }}
-    </p>
-    <button @click="changeValue">Change Value</button>
+  <hr style='border: solid yellow 3px' />
+  <div class='row'>
+    <h2 class='npsfont q-mx-lg q-mt-lg q-mb-none' style='color: #282828'>원하는 캐릭터로<br /> 마법처럼 변신!</h2>
+    <!--    <h4 class='npsfont q-my-none'>역할에 맞는 수많은 3D 아바타를 통해 아이들의 창의력과 집중력을 향상시킵니다</h4>-->
+    <div class='col-8 offset-2'>
+      <div class='row'>
+        <div class='col-6 felx justify-center q-mt-lg'>
+
+          <img src='src/assets/MyPageImages/iupic.jpg'
+               width='400px'
+               height='400px'
+               style='object-fit: cover; border-radius: 25px' />
+        </div>
+        <div class='col-6 flex justify-center items-center'>
+          <h4 class='npsfont'>마법같은 이야기와 함께<br />창의력과 상상력을 키워보세요!</h4>
+
+        </div>
+
+      </div>
+
+    </div>
+
   </div>
+  <hr style='border: solid yellow 3px' />
 </template>
 
 <script setup>
-  import {ref, watch} from 'vue';
 
-  const items = ref([
-    {id: 1, name: 'Item 1', value: {count: 10}},
-    {id: 2, name: 'Item 2', value: {count: 20}},
-    {id: 3, name: 'Item 3', value: {count: 30}},
-  ]);
-
-  const changeValue = () => {
-    // 임의의 아이템의 값을 변경
-    const randomIndex = Math.floor(Math.random() * items.value.length);
-    items.value[randomIndex].value.count += 10;
-  };
-
-  watch(items, (newItems, oldItems) => {
-    // 객체 배열의 값이 변경될 때 실행되는 콜백
-    console.log('Items changed:', newItems);
-    console.log('Old items:', oldItems);
-  }, {deep: true});
 </script>
 
 <style scoped>
--- Frontend/src/components/MainPageComponents/ElementTwo.vue
+++ Frontend/src/components/MainPageComponents/ElementTwo.vue
@@ -1,11 +1,33 @@
+<template>
+  <hr style='border: solid yellow 3px' />
+  <div class='row'>
+    <h2 class='npsfont q-mx-lg q-mt-lg q-mb-none' style='color: #282828'>원하는 캐릭터로<br /> 마법처럼 변신!</h2>
+    <!--    <h4 class='npsfont q-my-none'>역할에 맞는 수많은 3D 아바타를 통해 아이들의 창의력과 집중력을 향상시킵니다</h4>-->
+    <div class='col-8 offset-2'>
+      <div class='row'>
+
+        <div class='col-6 flex justify-center items-center'>
+          <h4 class='npsfont'>마법같은 이야기와 함께<br />창의력과 상상력을 키워보세요!</h4>
+
+        </div>
+        <div class='col-6 felx justify-center q-mt-lg'>
+
+          <img src='src/assets/MyPageImages/winter.png'
+               width='400px'
+               height='400px'
+               style='object-fit: cover; border-radius: 25px' />
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <hr style='border: solid yellow 3px' />
+</template>
+
 <script setup>
 
 </script>
-
-<template>
-  <hr style="border: solid green 3px"/>
-  <h1>2번 엘리먼트</h1>
-</template>
 
 <style scoped>
 
