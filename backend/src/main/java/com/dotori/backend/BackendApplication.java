61b4bf398f0a3314b55afa03b3e3343329937ea3
문준형 <moonabcd@naver.com>
2024-02-06 12:10:51 +0900

    fix : redis에 session저장안함

--- backend/src/main/java/com/dotori/backend/BackendApplication.java
+++ backend/src/main/java/com/dotori/backend/BackendApplication.java
@@ -3,11 +3,9 @@ package com.dotori.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableRedisHttpSession
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
