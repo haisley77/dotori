054b7af618ae8ee4868798ad4dd3ad638a34b66d
문준형 <moonabcd@naver.com>
2024-02-06 15:04:45 +0900

    chore : text 레디스제거2

--- backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-	// @Import(TestRedisConfig.class) // TestRedisConfig를 로드하기 위한 어노테이션
 class BackendApplicationTests {
 
 	@Test
