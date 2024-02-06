7e8397ea54cde8ac70fc6391a06bb9ef0d1e3104
jh <bndgc@naver.com>
2024-02-06 09:06:07 +0900

    mod: Room 가져와서 video 저장하도록 수정

--- backend/src/main/java/com/dotori/backend/domain/video/service/VideoService.java
+++ backend/src/main/java/com/dotori/backend/domain/video/service/VideoService.java
@@ -2,10 +2,13 @@ package com.dotori.backend.domain.video.service;
 
 import java.io.File;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dotori.backend.domain.room.model.entity.Room;
+import com.dotori.backend.domain.room.repository.RoomRepository;
 import com.dotori.backend.domain.video.model.dto.VideoSceneUploadRequest;
 
 import lombok.RequiredArgsConstructor;
@@ -18,21 +21,19 @@ public class VideoService {
 	private final VideoUploadService videoUploadService;
 	private final VideoManageService videoManageService;
 
-	// TODO front 기능 개발에 따라 차후 수정
+	private final RoomRepository roomRepository;
+
 	@Transactional
 	public boolean uploadSceneVideo(VideoSceneUploadRequest videoSceneUploadRequest) {
 		File uploadSceneVideo = videoUploadService.uploadChunkFiles(videoSceneUploadRequest);
 		if (uploadSceneVideo == null) {
 			return false;
 		}
-		// TODO room 개발 완료시 수정
-		// Room room = roomService.getRoom(videoSceneUploadRequest.get(roomId));
-		Room room = null;
-		videoManageService.saveSceneVideo(
-			room,
-			videoSceneUploadRequest.getSceneOrder(),
-			uploadSceneVideo.toString()
-		);
+
+		Room room = roomRepository.findById(videoSceneUploadRequest.getRoomId())
+			.orElseThrow(EntityNotFoundException::new);
+
+		videoManageService.saveSceneVideo(room, videoSceneUploadRequest.getSceneOrder(), uploadSceneVideo.toString());
 		return true;
 	}
 }
