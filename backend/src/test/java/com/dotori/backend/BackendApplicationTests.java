2a5a45d09f7e4d78f79c29021c1628e6bdd22920
문준형 <moonabcd@naver.com>
2024-02-08 21:28:44 +0900

    rebase : 3

--- backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+	// @Import(TestRedisConfig.class) // TestRedisConfig를 로드하기 위한 어노테이션
 class BackendApplicationTests {
 
 	@Test
