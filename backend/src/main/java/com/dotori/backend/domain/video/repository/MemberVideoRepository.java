d11e28c404177074ba0713a1e3f68fcf14fa04ff
bufflect <bndgc@naver.com>
2024-02-09 20:17:11 +0900

    add: MemberVideoRepository

---
+++ backend/src/main/java/com/dotori/backend/domain/video/repository/MemberVideoRepository.java
@@ -0,0 +1,8 @@
+package com.dotori.backend.domain.video.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dotori.backend.domain.member.model.entity.MemberVideo;
+
+public interface MemberVideoRepository extends JpaRepository<MemberVideo, Long> {
+}
