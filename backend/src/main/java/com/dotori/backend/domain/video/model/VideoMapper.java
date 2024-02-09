db57e9b47a9d9dedc90e9c39c0f9c89d97bd261b
bufflect <bndgc@naver.com>
2024-02-09 20:16:23 +0900

    mod: videoDto -> toVideo

--- backend/src/main/java/com/dotori/backend/domain/video/model/VideoMapper.java
+++ backend/src/main/java/com/dotori/backend/domain/video/model/VideoMapper.java
@@ -4,6 +4,12 @@ import com.dotori.backend.domain.video.model.dto.VideoDto;
 import com.dotori.backend.domain.video.model.entity.Video;
 
 public class VideoMapper {
+	public static Video toVideo(VideoDto videoDto) {
+		return Video.builder()
+			.path(videoDto.getPath())
+			.build();
+	}
+
 	public static VideoDto toVideoDto(Video video) {
 		return new VideoDto(video.getVideoId(), video.getPath());
 	}
