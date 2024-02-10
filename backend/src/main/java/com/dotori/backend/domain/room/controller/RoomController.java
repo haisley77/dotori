4ff945e9dc2b8f15337678a40b0f257f98f772e3
이하은 <hahi1108@gmail.com>
2024-02-10 19:52:24 +0900

    mod : update 동작 patch 매핑

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -166,8 +167,7 @@ public class RoomController {
 		}
 	}
 
-	// @PatchMapping("/update/{roomId}")
-	@PostMapping("/update/{roomId}")
+	@PatchMapping("/update/{roomId}")
 	public ResponseEntity<Map<String, String>> updateRoom(@PathVariable("roomId") Long roomId,
 		@RequestBody(required = true) RoomDto roomInfo) {
 		Map<String, String> resultData = new HashMap<>();
