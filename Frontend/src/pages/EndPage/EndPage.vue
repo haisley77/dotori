08c1762dc5e8d0658d22b016bd5b0a4182473bad
jinooyang <jinooyang@gmail.com>
2024-02-12 14:58:19 +0900

    feat : endpage video

--- Frontend/src/pages/EndPage/EndPage.vue
+++ Frontend/src/pages/EndPage/EndPage.vue
@@ -5,13 +5,18 @@
   <div class="q-pa-lg row">
     <div
          class="q-ma-lg q-pa-lg col-8 offset-2">
-      <div class="row">
+      <div class="row flex justify-center ">
         <!--        <div class="col-2"></div>-->
-        <div class="col-3 flex justify-center items-center q-pa-sm " v-for="item in 4" key=item>
-          <div class="background-green flex items-center justify-center">
-            <img class="q-ma-md" src="../../assets/DotoriImages/acorn_character_img.png" width="80%">
+          <div class='items-center q-ma-none q-pa-none'>
+              <div v-if='ovstore.mainStreamManager'>
+                  <ov-video style="" :stream-manager='ovstore.mainStreamManager'
+                            :id='ovstore.mainStreamManager.stream.streamId' />
+              </div>
+              <div v-for='sub in ovstore.subscribers' :key='sub.stream.connection.connectionId'>
+                  <ov-video :stream-manager='sub'
+                            :id='sub.stream.streamId' />
+              </div>
           </div>
-        </div>
         <!--        <div class="col-2"></div>-->
       </div>
 
@@ -24,14 +29,51 @@
         <h4 class="npsfont q-mb-md q-mt-none">결과는 마이페이지에서 확인하세요!</h4>
       </div>
       <div class="row flex justify-center">
-        <q-btn unelevated rounded color="my-brown" class="q-mx-sm" size="1.5em" label="홈으로" />
-        <q-btn unelevated rounded color="my-brown" class="q-mx-sm" size="1.5em" label="마이페이지" />
+        <q-btn unelevated rounded color="my-brown" class="q-mx-sm" size="1.5em" label="홈으로" @click='moveToMainPage'/>
+        <q-btn unelevated rounded color="my-brown" class="q-mx-sm" size="1.5em" label="마이페이지" @click='moveToMyPage'/>
       </div>
     </div>
   </div>
 </template>
 <script setup>
   import Header from 'components/CommonComponents/Header.vue';
+  import OvVideo from 'components/RecordingPageComponents/OvVideo.vue';
+  import {useOpenViduStore} from 'stores/openvidu';
+  import {onMounted} from 'vue';
+  import {useRouter} from 'vue-router';
+  const router = useRouter();
+  const ovstore = useOpenViduStore();
+
+  const moveToMainPage = ()=>{
+      unpublish();
+      ovstore.session.disconnect();
+      router.push('/');
+  };
+  const moveToMyPage = ()=>{
+      unpublish();
+      ovstore.session.disconnect();
+      router.push('/my-page/info');
+  }
+
+  onMounted(()=>{
+      const publisher = ovstore.OV.initPublisher(undefined, {
+          // audioSource: myRealCanvas.value.captureStream().getAudioTracks()[0], // The source of audio. If undefined default microphone
+          // videoSource: myRealCanvas.value.captureStream().getVideoTracks()[0], // The source of video. If undefined default webcam
+          // videoSource: canvasStream, // The source of video. If undefined default webcam
+          // videoSource: undefined, // The source of video. If undefined default webcam
+          publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+          publishVideo: true, // Whether you want to start publishing with your video enabled or not
+          resolution: '640x480', // The resolution of your video
+          frameRate: 30, // The frame rate of your video
+          insertMode: 'APPEND', // How the video is inserted in the target element 'video-container'
+          // mirror: false, // Whether to mirror your local video or not
+      });
+      ovstore.publish(publisher);
+
+  });
+  const unpublish = () => {
+      if (ovstore.isPublished) ovstore.unpublish();
+  };
 </script>
 
 <style scoped>
