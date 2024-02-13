20b40514c31560d0eff5cb4f997deab1962a6d4c
yeonmi <ar77gt@daum.net>
2024-02-13 15:16:28 +0900

    design

--- Frontend/src/pages/ListPage/BookListPage.vue
+++ Frontend/src/pages/ListPage/BookListPage.vue
@@ -1,27 +1,45 @@
 <template>
   <headerbooklist/>
+
+  <div class='row q-mb-md'>
+    <div class='search row flex justify-center items-center' style='width: 100%;'>
+      <q-input
+        class=''
+        standout rounded dense placeholder='검색'
+        bg-color='white'
+        :text-color="'black'"
+        style='width: 50%; border: rgba(218, 201, 157, 0.87) solid 3.5px; border-radius: 50px;'
+        v-model="searchQuery"
+        @keyup.enter="search"
+      >
+      </q-input>
+      <q-btn
+        class=''
+        icon='search'
+        round
+        size='lg'
+        flat
+        unelevated
+        style='color: #C7A96E; font-weight: bolder'
+        @click="search"
+      />
+    </div>
+  </div>
+
   <q-page class="page-container">
     <div class='row q-col-gutter-x-md q-col-gutter-y-md'>
       <div v-for='(book,index) in books' :key='book.bookId' class='col-3 q-pa-md'>
-        <div
-          class="book-container"
-          @mouseover="showModal(book)"
-          @mouseleave="hideModal"
-        >
-          <Book class='book-component' :bookdetail="book"></Book>
+        <div class="book-container" @mouseover="showModal(book)" @mouseleave="hideModal">
+            <Book class='book-component' :bookdetail="book"></Book>
             <!-- 모달 내용 -->
-            <q-card class="my-card " :class="{'modal-left':(index%4)===3, 'modal-right':(index%4)!==3}" v-if="selectedBook === book">
-<!--              <img :src="book.bookImg">-->
-              <q-card-section>
-                <div class="text-h6 npsfont">연극을 하려면 {{ book.roleCnt }} 명의 친구들이 필요해요!</div>
-              </q-card-section>
-              <q-card-section class="q-pt-none npsfont">
-                '방만들기' 버튼을 눌러 친구들을 모아볼까요?
-              </q-card-section>
-            </q-card>
+<!--            <q-card class="my-card modal-left" v-if="selectedBook === book">-->
+<!--              <q-card-section>-->
+<!--                <div class="text-h7 npsfont">연극을 하려면</div>-->
+<!--                <div class="text-h7 npsfont">{{ book.roleCnt }} 명의 친구들이 필요해요!</div>-->
+<!--              </q-card-section>-->
+<!--            </q-card>-->
         </div>
       </div>
-
     </div>
   </q-page>
 </template>
@@ -38,6 +56,7 @@
     setup() {
       const books = ref([]);
       const selectedBook = ref(null);
+      const searchQuery = ref('')
 
       onMounted(() => {
         fetchBooks();
@@ -51,6 +70,12 @@
         } catch (error) {
           console.error('Error fetching books:', error);
         }
+      };
+
+      const search = () => {
+        books.value = books.value.filter(book => {
+          return book.title.includes(searchQuery.value);
+        });
       };
 
       const showModal = (book) => {
@@ -68,14 +93,22 @@
         selectedBook,
         showModal,
         hideModal,
+        searchQuery,
+        search,
       };
     },
   };
 </script>
 
 <style lang='scss' scoped>
+
+  .book-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
   .page-container {
-    margin-top: 80px;
+    margin-top: 30px;
     margin-left : 50px;
 
   }
@@ -89,27 +122,16 @@
     transform: scale(1.1); /* 1.1 배 확대 */
     transition: transform 0.3s ease;
   }
-  .modal-right {
-    position: absolute;
-    z-index: 9999;
-    top: calc(30% + 10px);
-    left: calc(0% + 20px);
-    width: 300px; /* 모달의 너비 */
-    height: 180px; /* 모달의 높이 */
-    background-color: white;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 30px; /* 테두리를 둥글게 만듭니다. */
-    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-  }
+
   .modal-left {
       position: absolute;
       z-index: 9999;
+      bottom:10px;
       top: calc(10% + 10px);
-      left: calc(-100% + 20px);
-      width: 300px; /* 모달의 너비 */
-      height: 180px; /* 모달의 높이 */
-      background-color: white;
+      left: calc(0% + 0px);
+      width: 280px; /* 모달의 너비 */
+      height: 80px; /* 모달의 높이 */
+      background-color: #6fe286;
       padding: 10px;
       border: 1px solid #ccc;
       border-radius: 30px; /* 테두리를 둥글게 만듭니다. */
