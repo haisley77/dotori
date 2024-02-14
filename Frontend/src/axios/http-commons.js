7741c0c389433cb22cbf3217c8ae727ae3c27e01
doongdang <whtjrgus0428@gmail.com>
2024-02-14 12:50:29 +0900

    fix : conflict

--- Frontend/src/axios/http-commons.js
+++ Frontend/src/axios/http-commons.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 function localAxios() {
   const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://dotori.online',
     withCredentials: true,
     headers: {
       "Content-Type": "application/json;charset=utf-8",
@@ -17,12 +17,50 @@ function localAxios() {
   instance.defaults.headers.get["Content-Type"] = "application/json";
   instance.defaults.headers.delete["Content-Type"] = "application/json";
   // instance.defaults.headers.put["Content-Type"] = "application/json";
+
+  instance.interceptors.response.use(
+    response => response,
+    async error => {
+      if (error.response.status === 401 && !error.config._retry) {
+        const errorMessage = error.response.data.error;
+        if (errorMessage === 'Access Token is invalid') {
+          error.config._retry = true; // 재시도 플래그 설정
+          console.log('토큰갱신진입');
+          const newAccessToken = await refreshToken(); // 액세스 토큰 갱신
+          console.log('토큰갱신완료');
+          error.config.headers['accessToken'] = newAccessToken;
+          return instance(error.config); // 요청 재시도
+        }
+      }
+      return Promise.reject(error);
+    },
+  );
+
+  async function refreshToken() {
+    try {
+      const response = await instance.get(
+        'https://dotori.online/api/members/reaccesstoken',
+      );
+      console.log('response', response);
+      const accessTokenPrefix = 'accessToken:';
+      const startIndex =
+        response.data.indexOf(accessTokenPrefix) +
+        accessTokenPrefix.length;
+      const newAccessToken = response.data.substring(startIndex);
+      return newAccessToken;
+    } catch (error) {
+      console.error('토큰 갱신 실패', error);
+      throw error;
+    }
+  }
+
+
   return instance;
 }
 
 function imgAxios() {
   const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://dotori.online',
     withCredentials: true,
     headers: {
       "Content-Type": "multipart/form-data;charset=utf-8",
